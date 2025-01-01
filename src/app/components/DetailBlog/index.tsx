49537999551a46ce4637a1062fdb1ad087373374
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-01-01 22:46:57 +0500

    detailblog responsiveness

--- src/app/components/DetailBlog/index.tsx
+++ src/app/components/DetailBlog/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { urlFor } from '@/sanity/lib/image';
 import { dataType } from '@/app/components/Home';
+import Image from 'next/image';
 
 interface DetailBlogProps {
     data: dataType[] | undefined;
@@ -9,10 +11,18 @@ const DetailBlog: React.FC<DetailBlogProps> = ({ data }) => {
     return (
         <div>
             {data && data.map((v, i) => <div key={i}>
-                <h1>{v.post_title}</h1>
-                <p>{v.post_description}</p>
-            </div>)}
-        </div>
+                <h1 className="font-bold">{v.post_title}</h1>
+                <span className="flex flex-col md:flex-row ">
+                    <p>{v.post_description}</p>
+                    <Image
+                        src={urlFor(v.post_image).url()}
+                        alt={v.post_title}
+                        width="200"
+                        height="200" />
+                </span>
+            </div>)
+            }
+        </div >
     )
 }
 
