5a8b9289ac9303ed7ba77bcda5cd42d5b2fe9874
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-01-01 21:25:40 +0500

    Transferred the components to layout.tsx fiel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Navbar from './components/Navbar'
 import Home from './components/Home'
-import Footer from './components/Footer'
 
 const Page = () => {
   return (
     <div>
-      <Navbar />
       <Home />
-      <Footer />
     </div>
   )
 }
