2cc297decef7e4b1d32933dd7b2f23a322b62c47
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-01-01 21:23:14 +0500

    Made home page responsiveness

--- src/app/components/Home/index.tsx
+++ src/app/components/Home/index.tsx
@@ -32,22 +32,24 @@ const Home = () => {
     return (
         <div>
             {fetchedData?.length === 0 ? <h1>Loading...</h1> :
-                <div>
+                <div className='grid text-[10px] sm:text-[12px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 px-standardSize'>
                     {
-                        fetchedData && fetchedData.map((v: dataType, i) => <div className="flex justify-between items-center bg-gray-50 text-black p-2 my-2" key={i}>
+                        fetchedData && fetchedData.map((v: dataType, i) => <div className="flex justify-between items-center bg-gray-50 text-black p-2" key={i}>
                             <span>
                                 <h2
-                                    className="font-bold uppercase text-xl"
+                                    className="font-bold uppercase text-sm sm:text-[16px] md:text-[14px]"
                                 >{v.post_title}</h2>
-                                <p
-                                    className="mx-5"
-                                >{v.post_description}</p>
+                                <span
+                                    className="space-y-2"
+                                >
+                                    <p>{v.post_description}</p>
+                                    <Image
+                                        src={urlFor(v.post_image).url()}
+                                        alt={v.post_title}
+                                        width="120"
+                                        height="120" />
+                                </span>
                             </span>
-                            <Image
-                                src={urlFor(v.post_image).url()}
-                                alt={v.post_title}
-                                width="120"
-                                height="120" />
                         </div>)
                     }
                 </div>
