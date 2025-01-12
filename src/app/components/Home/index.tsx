4c9ad201bd97131c783efea84d5101a1e9194421
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-01-13 01:00:56 +0500

    build made successful

--- src/app/components/Home/index.tsx
+++ src/app/components/Home/index.tsx
@@ -3,8 +3,9 @@
 import { client } from '@/sanity/lib/client';
 import { urlFor } from '@/sanity/lib/image';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation'; // Change this import
 import React, { useEffect, useState } from 'react'
+
 
 export interface dataType {
     _id: string,
@@ -18,6 +19,7 @@ export interface dataType {
 }
 
 const Home = () => {
+
     const [fetchedData, setFetchedData] = useState<dataType[]>();
     useEffect(() => {
         const fetchFunction = async () => {
@@ -40,24 +42,41 @@ const Home = () => {
             await client.delete(postId);
             console.log(`Post ${postId} deleted successfully!`);
         } catch (error) {
-            console.error('Error deleting post:', error?.message);
+            // console.error('Error deleting post:', error?.message);
+            console.error('Error deleting post:', error);
         }
     }
 
-    const updatePost = async (postId: string, updatedData) => {
-        try {
-            await client.patch(postId) // Target the post by ID
-                .set(updatedData)        // Set new data
-                .commit();               // Commit changes
+    // const updatePost = async (postId: string, updatedData) => {
+    //     try {
+    //         await client.patch(postId) // Target the post by ID
+    //             .set(updatedData)        // Set new data
+    //             .commit();               // Commit changes
 
-            console.log(`Post ${postId} updated successfully!`);
-        } catch (error) {
-            console.error('Error updating post:', error.message);
-        }
-    };
+    //         console.log(`Post ${postId} updated successfully!`);
+    //     } catch (error) {
+    //         console.error('Error updating post:', error.message);
+    //     }
+    // };
 
-    const updateForm = (data: any) => {
+    // const updateForm = (data: any) => {
+    //     console.log('data...', data);
+    //     // const queryString = new URLSearchParams({ data: JSON.stringify(data) }).toString();
+    //     // router.push(`/form?${queryString}`);
+
+    //     router.push({
+    //         pathname: '/form',
+    //         query: { data }
+    //     });
+
+    //     console.log('.....')
+    // }
+
+    const updateForm = (data: dataType) => {
+        console.log('====================================');
         console.log('data...', data);
+        console.log('====================================');
+        router.push(`/form?id=${data}`);
     }
 
     return (
@@ -65,9 +84,11 @@ const Home = () => {
             {fetchedData?.length === 0 ? <h1>Loading...</h1> :
                 <div className='grid text-[10px] sm:text-[12px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 px-standardSize'>
                     {
-                        fetchedData && fetchedData.map((v: dataType, i) => <div className="relative">
+                        fetchedData && fetchedData.map((v: dataType, i) => <div key={i} className="relative">
                             <button className="bg-red-700 absolute top-2 right-14 text-white p-1 rounded cursor-pointer" onClick={() => deleteCard(v._id)}>Delete</button>
-                            <button className="bg-green-700 absolute top-2 right-2 text-white p-1 rounded cursor-pointer" onClick={() => { updateForm(v) }}>Update</button>
+                            <button className="bg-green-700 absolute top-2 right-2 text-white p-1 rounded cursor-pointer"
+                                onClick={() => { updateForm(v) }}
+                            >Update</button>
                             <div className="flex justify-between items-center bg-gray-50 text-black p-5" onClick={() => handleNavigate(v._id)}>
                                 <span>
                                     <h2
--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+"use client";
+// import { useEffect } from 'react';
 import FormComponent from '../components/Form'
+// import { useRouter } from 'next/navigation';
+
+// interface FormProps {
+//     // searchParams: { [key: string]: string | undefined };
+// }
 
 const Form = () => {
+    // const Form = ({ searchParams }: FormProps) => {
+
+    // const router = useRouter();
+    // const { id, name } = router.query;
+
+    // const [userData, setUserData] = useState<{ id: number; name: string; age: number } | null>(null);
+
+    // useEffect(() => {
+    //     console.log("searchParams...", searchParams)
+    // }, [])
+
     return (
         <div>
             <FormComponent />
