1be51aa73d3230d3d8aecc0f085e59f3de24a717
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-18 17:56:05 +0500

    padding radius applied

--- src/app/Location/page.tsx
+++ src/app/Location/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Location = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center h-screen p-2 rounded-full">
             <input placeholder="Enter your location" />
         </div>
     )
