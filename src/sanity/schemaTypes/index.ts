c71b72f99378c46044faf4064177cfed3821b62a
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-18 17:46:01 +0500

    Job schema imported in index.ts file

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -4,7 +4,8 @@ import postType from './postType';
 import service from './service';
 import location from './location';
 import agent from './agent';
+import job from './job';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [postType, service, location, agent],
+  types: [postType, service, location, agent, job],
 }
