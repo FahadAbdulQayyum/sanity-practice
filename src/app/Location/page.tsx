7c38e343cd6920eb647d286e8e35c07cf73efbc7
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-18 18:03:30 +0500

    bg color applied with cursor noen

--- src/app/Location/page.tsx
+++ src/app/Location/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Location = () => {
     return (
-        <div className="flex justify-center items-center h-screen p-2 rounded-full">
+        <div className="flex justify-center items-center h-screen p-2 rounded-full bg-white cursor-none">
             <input placeholder="Enter your location" />
         </div>
     )
