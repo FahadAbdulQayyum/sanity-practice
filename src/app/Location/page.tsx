7af038fa6ad0e7deaa2ed163b93843c7b64b8c1e
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-18 19:36:51 +0500

    location data fetched and displayed

--- src/app/Location/page.tsx
+++ src/app/Location/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+"use client"
+import { client } from '@/sanity/lib/client'
+import React, { useEffect, useState } from 'react'
+
+interface locationType {
+    name: string
+}
 
 const Location = () => {
+
+    const [location, setLocation] = useState<locationType[]>();
+
+    useEffect(() => {
+        const fetchLocation = async () => {
+            const fetchLocationData = await client.fetch(`
+                *[_type=='location']{ name }
+                `)
+            console.log('fetched location data', fetchLocationData)
+            setLocation(fetchLocationData);
+        }
+        fetchLocation();
+    }, [])
+
     return (
-        <div className="flex justify-center items-center h-screen p-2 rounded-full bg-white cursor-none">
-            <input placeholder="Enter your location" className='p-2 rounded-full cursor-none' />
+        <div className="flex flex-col justify-center items-center h-screen ">
+            <input placeholder="Enter your location" className='flex p-2 rounded-t-lg cursor-none text-black' />
+            <div
+                className="bg-white text-black h-32 overflow-y-scroll scrollbar-hide"
+            >
+                {location?.map((v: locationType, i: number) => <div key={i} className="flex flex-col px-[18px] hover:bg-gray-200 cursor-pointer my-2">{v.name}</div>)}
+            </div>
         </div>
     )
 }
