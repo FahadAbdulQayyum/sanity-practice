29ef579f0e629fae24b86249dc73230a837a40ed
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-18 17:47:16 +0500

    /location address creaed

---
+++ src/app/Location/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Location = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <input placeholder="Enter your location" />
+        </div>
+    )
+}
+
+export default Location
