57e6d22c48907845637bd857fb435e2cdc348d85
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-18 16:00:50 +0500

    location schema defined

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from 'sanity'
 
 import postType from './postType';
 import service from './service';
+import location from './location';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [postType, service],
+  types: [postType, service, location],
 }
